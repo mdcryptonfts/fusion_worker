45cdaf4fe98bb37f5fc64e3b76f4c03ce21b1cfe
Admin <admin@Admins-MacBook-Pro.local>
2024-03-23 14:05:15 -0700

    claimbgm

---
+++ claimbgmvote.js
@@ -0,0 +1,52 @@
+const config = require('./config.json');
+const { Session } = require('@wharfkit/session');
+const { WalletPluginPrivateKey } = require('@wharfkit/wallet-plugin-privatekey')
+
+const claimbgmvote = async () => {
+  let success = false;
+
+  for (let endpoint of config.chain_api.endpoints) {
+    try {
+
+      const WAX_CHAIN = { id: config.chain_api.chain_id, 
+                          url: endpoint }  
+
+      const session = new Session({
+          actor: config.permission.wallet,
+          permission: config.permission.permission_name,
+          chain: WAX_CHAIN,
+          walletPlugin: new WalletPluginPrivateKey(config.permission.private_key)
+      })                          
+
+      const apiCallPromise = session.transact({
+        actions: [{
+          account: config.contracts.pol_contract,
+          name: 'claimbgmvote',
+          authorization: [{
+            actor: config.permission.wallet,
+            permission: config.permission.permission_name,
+          }],
+          data: {
+          }
+        }]
+      }, {
+        blocksBehind: config.blocks_behind,
+        expireSeconds: config.expire_seconds
+      });
+
+      const result = await Promise.race([apiCallPromise, new Promise((_, reject) => setTimeout(() => reject(new Error('Timeout')), config.transaction_timeout))]);
+
+      console.log("\n\nclaimbgmvote submission successful");
+      success = true;
+      return success;
+    } catch (e) {
+      console.log(`error submitting claimbgmvote: ${e}`);
+    }
+  }
+
+  return success;
+};
+
+module.exports = {
+  claimbgmvote
+};
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const config = require('./config.json');
 const { submitAddLiquidityTx } = require('./add_liquidity');
+const { claimbgmvote } = require('./claimbgmvote');
 
 
 const runApp = async () => {
@@ -9,7 +10,12 @@ const runApp = async () => {
 	/** @submitAddLiquidityTx
 	 *  every 6 hours, try adding liquidity (it can be called once a day)
 	 */
-	setInterval(() => submitAddLiquidityTx(), (60 * 1000) );
+	setInterval(() => submitAddLiquidityTx(), (60 * 60 * 6 * 1000) );
+
+	/** @claimgbmvote
+	 *  every 6 hours + 1 minute, try adding claimbgmvote (it can be called once a day)
+	 */
+	setInterval(() => claimbgmvote(), (60 * 1000) + 1 );	
 
     process.on('SIGINT', () => {
         process.exit();
