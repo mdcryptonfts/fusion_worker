f5d0d0eb8f6b6f5a9988e13fca6fa28bcc55a075
Admin <admin@Admins-MacBook-Pro.local>
2024-03-23 13:48:28 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const config = require('./config.json');
+const { submitAddLiquidityTx } = require('./add_liquidity');
+
+
+const runApp = async () => {
+
+	console.log("App is running")
+					      
+	/** @submitAddLiquidityTx
+	 *  every 6 hours, try adding liquidity (it can be called once a day)
+	 */
+	setInterval(() => submitAddLiquidityTx(), (60 * 60 * 6 * 1000) );
+
+    process.on('SIGINT', () => {
+        process.exit();
+    });	
+	
+};
+
+runApp();
